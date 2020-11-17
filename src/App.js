408bf0ce054fe02cacdf71df37fdbe797d4f59f6
Jatin Jasoliya <>
2020-11-17 09:21:25 -0500

    New routing updates

--- src/App.js
+++ src/App.js
@@ -1,12 +1,20 @@
 import React from "react";
 import "./App.css";
+import { Switch, Route } from "react-router-dom";
 import HomePage from "./components/pages/homepage/homepage.component";
+
+const HatsPage = () => (
+  <div>
+    <h1>HatPages</h1>
+  </div>
+);
 
 function App() {
   return (
-    <div>
-      <HomePage />
-    </div>
+    <Switch>
+      <Route exact path="/" component={HomePage}></Route>
+      <Route path="/hats" component={HatsPage}></Route>
+    </Switch>
   );
 }
 
