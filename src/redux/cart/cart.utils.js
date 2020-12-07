fef25466e3346af4acf21ffc7af67d8772a1ef8f
Jatin Jasoliya <>
2020-12-07 10:28:23 -0500

    Update cart and shop data

---
+++ src/redux/cart/cart.utils.js
@@ -0,0 +1,20 @@
+// Utility function allow us to keep our file clean and organize function that we may
+// need in multiple files in one loaction
+
+// Adding multiple item in cart
+export const addItemToCart = (cartItems, cartItemToAdd) => {
+  const existingCartItem = cartItems.find(
+    (cartItem) => cartItem.id === cartItemToAdd.id
+  );
+
+  if (existingCartItem) {
+    //Return new version of our state
+    return cartItems.map((cartItem) =>
+      cartItem.id === cartItemToAdd.id
+        ? { ...cartItem, quantity: cartItem.quantity + 1 }
+        : cartItem
+    );
+  }
+
+  return [...cartItems, { ...cartItemToAdd, quantity: 1 }];
+};
